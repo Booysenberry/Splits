068448f78275a6624794e52cc8c5889eed3044c1
Brad <brad.booysen@gmail.com>
2020-02-28 07:35:12 +1300

    Added race model

---
+++ Splits/Model.swift
@@ -0,0 +1,21 @@
+//
+//  Model.swift
+//  Splits
+//
+//  Created by Brad Booysen on 28/02/20.
+//  Copyright © 2020 Booysenberry. All rights reserved.
+//
+
+import Foundation
+
+struct Race {
+    var swimDistance: Double?
+    var swimTime: Int?
+    var t1: Int
+    var bikeDistance: Double?
+    var bikeTime: Int?
+    var t2: Int
+    var runDistance: Double?
+    var runTime: Int?
+    var totalTime: Int?
+}
