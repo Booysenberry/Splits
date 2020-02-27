7d63b7862a463fcaa7dc0b7828977928af589fee
Brad <brad.booysen@gmail.com>
2020-02-28 07:25:45 +1300

    Wire up UI

--- Splits/CalculatorView.swift
+++ Splits/CalculatorView.swift
@@ -2,37 +2,18 @@
 //  CalculatorView.swift
 //  Splits
 //
-//  Created by Brad Booysen on 27/02/20.
+//  Created by Brad Booysen on 28/02/20.
 //  Copyright © 2020 Booysenberry. All rights reserved.
 //
 
 import UIKit
 
-class CalculatorView: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    @IBOutlet weak var tableView: UITableView!
+class CalculatorView: UIViewController {
+
+    @IBOutlet weak var distanceSelector: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
 
-    }
-    
-    // MARK: Tableview Data
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SplitCell") as! SplitsCell
-        
-        return cell
     }
 }
---
+++ Splits/DisciplinesTable.swift
@@ -0,0 +1,47 @@
+//
+//  DisciplinesTable.swift
+//  Splits
+//
+//  Created by Brad Booysen on 28/02/20.
+//  Copyright © 2020 Booysenberry. All rights reserved.
+//
+
+import UIKit
+
+class DisciplinesTable: UITableViewController {
+    
+    @IBOutlet weak var swimPace: UILabel!
+    @IBOutlet weak var swimTime: UILabel!
+    @IBOutlet weak var swimDistance: UILabel!
+    @IBOutlet weak var swimSlider: UISlider!
+    
+    @IBOutlet weak var t1Pace: UILabel!
+    @IBOutlet weak var t1Time: UILabel!
+    @IBOutlet weak var t1Slider: UISlider!
+    
+    
+    @IBOutlet weak var bikePace: UILabel!
+    @IBOutlet weak var bikeTime: UILabel!
+    @IBOutlet weak var bikeDistance: UILabel!
+    @IBOutlet weak var bikeSlider: UISlider!
+    
+    @IBOutlet weak var t2Pace: UILabel!
+    @IBOutlet weak var t2Time: UILabel!
+    @IBOutlet weak var t2Slider: UISlider!
+    
+    @IBOutlet weak var runPace: UILabel!
+    @IBOutlet weak var runTime: UILabel!
+    @IBOutlet weak var runDistance: UILabel!
+    @IBOutlet weak var runSlider: UISlider!
+    
+    @IBOutlet weak var totalTime: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Remove unused rows
+        tableView.tableFooterView = UIView()
+
+    }
+}
